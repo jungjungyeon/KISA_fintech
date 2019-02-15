b6fc11b53af3a9ed0f9f36d4bf5a142e920bfc55
KimJungYeon <jylisa@naver.com>
2019-02-15 10:40:54 +0900

    add

---
+++ kisa_example/example.js
@@ -0,0 +1,12 @@
+function bigger(value) {
+    var base = 10;
+    if(value > 10) {
+        return true;
+    }
+    else  {
+        return false;
+    }
+}
+
+console.log(bigger(20));
+console.log(bigger(5));
